bd343547d3a2f5d6e85181939bbd5974f0e87c2d
Serdar Esenov <83946397+esenoff@users.noreply.github.com>
2022-07-28 18:49:25 +0300

    add JS part

--- Dog.js
+++ Dog.js
@@ -1,6 +1,32 @@
 
-class Dog{
+class Dog {
     constructor(data){
         Object.assign(this, data)
+
+        this.like = this.hasBeenLiked;
+        this.dislike = this.hasBeenSwiped;
+    }
+
+    likeMatch(){
+        this.like = true;
+        this.dislike = false;
+    }
+
+    dislikeMatch(){
+        this.like = false;
+        this.dislike = true;
+    }
+
+    getNewMatch(){
+        const {avatar, name, age, bio, setBadge} = this
+        return `<img src=${avatar} alt="Dog image" class="main">
+        <img src=${this.like && !this.dislike ? "images/badge-like.png"
+        :!this.like && this.dislike ? "images/badge-nope.png"
+        :"#"} alt=" " class="badge">
+        <h2 class="name">${name}, ${age}</h2>
+        <p class="about">${bio}</p>`
+        
     }
 }
+
+export default Dog;
--- index.js
+++ index.js
@@ -1,2 +1,41 @@
-import dogs from "./data"
+import dogs from "./data.js"
+import Dog from "./Dog.js"
+
+
+function like(){
+    match.likeMatch()
+    initial()
+    setTimeout(()=>{
+        newMatch()
+        next = new Dog(dogs[Math.floor(Math.random( )*(dogs.length))])
+    },1000)
+}
+
+function dislike(){
+    match.dislikeMatch()
+    initial()
+    setTimeout(()=>{
+        newMatch()
+        next = new Dog(dogs[Math.floor(Math.random( )*(dogs.length))])
+    },1000)
+}
+
+ let match = new Dog(dogs[Math.floor(Math.random( )*(dogs.length))])
+ let next = new Dog(dogs[Math.floor(Math.random( )*(dogs.length))])
+
+ function initial(){
+    document.getElementById("content").innerHTML = match.getNewMatch()   
+ }
+
+ function newMatch(){
+    document.getElementById("content").innerHTML = next.getNewMatch() 
+    match = next 
+ }
+document.getElementById('btn-like').addEventListener("click", like);
+
+document.getElementById('btn-dislike').addEventListener("click", dislike);
+
+initial();
+
+
 
